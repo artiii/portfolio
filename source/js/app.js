ef05f4427a901d477895f25fdd3c0727fcb38842
Artur Shaydulin <art234562@gmail.com>
2016-10-02 05:40:12 +0300

    about without the map

--- source/js/app.js
+++ source/js/app.js
@@ -1,7 +1,7 @@
-(function() {
-  'use strict';
-
-  setTimeout(function() {
-    document.querySelector('.greating_picture').classList.add('m--show');
-  }, 1000);
-})();
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 8
+  });
+}
