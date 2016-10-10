05b03ea36300b19403fa35926d1a5e05d4759af0
Artur Shaydulin <art234562@gmail.com>
2016-10-10 11:04:20 +0300

    adaptive 95%

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -5,6 +5,10 @@ module.exports = function() {
     return $.gulp.src('./source/style/app.scss')
       .pipe($.gp.sourcemaps.init())
       .pipe($.gp.sass()).on('error', $.gp.notify.onError({ title: 'Style' }))
+      .pipe($.gp.cssUnit({
+        type     :    'px-to-rem',
+        rootSize :    16
+      }))
       .pipe($.gp.autoprefixer({ browsers: $.config.autoprefixerConfig }))
       .pipe($.gp.sourcemaps.write())
       .pipe($.gulp.dest($.config.root + '/assets/css'))
--- source/js/app.js
+++ source/js/app.js
@@ -1,7 +1,9 @@
-var map;
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
+  // Create a map object and specify the DOM element for display.
+  var map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: -34.397, lng: 150.644},
+    scrollwheel: false,
     zoom: 8
   });
 }
+
