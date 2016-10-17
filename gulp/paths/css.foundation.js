0ff870e298addad43bd7c6c3d5d7c2255fba58e2
Artur Shaydulin <art234562@gmail.com>
2016-10-17 11:33:36 +0300

    add slider, menu80%, preloader new

--- gulp/paths/css.foundation.js
+++ gulp/paths/css.foundation.js
@@ -2,6 +2,7 @@
 
 module.exports = [
   './node_modules/normalize.css/normalize.css',
-  './bower_components/qtip2/jquery.qtip.css'
+  './bower_components/qtip2/jquery.qtip.css',
+  './bower_components/animate.css/animate.min.css'
 
 ];
--- source/js/app.js
+++ source/js/app.js
@@ -3,132 +3,79 @@
 function initMap() {
   // Create a map object and specify the DOM element for display.
   var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
+    center: {lat: 59.998958, lng: 30.369851},
     scrollwheel: false,
-    zoom: 8
+    zoom: 15,
+    styles: [{"featureType":"all","elementType":"all","stylers":[{"visibility":"on"},{"hue":"#ff0000"}]},{"featureType":"all","elementType":"geometry","stylers":[{"saturation":"100"}]},{"featureType":"all","elementType":"geometry.stroke","stylers":[{"visibility":"on"},{"hue":"#ff0000"}]},{"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#444444"}]},{"featureType":"administrative.country","elementType":"labels.text.fill","stylers":[{"hue":"#ff0000"}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2f2f2"}]},{"featureType":"landscape.natural.terrain","elementType":"labels.text.stroke","stylers":[{"visibility":"on"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":-100},{"lightness":45}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#86a77a"},{"visibility":"on"}]}]
   });
 }
 
 /*preloader*/
-
-
-$(function () {
-
-  var imgs = [];
-
-  $.each($('*'), function () {
-    var
-        $this = $(this),
-        background = $this.css('background-image'),
-        img = $this.is('img');
-
-    if (background != 'none') {
-      var path = background.replace('url("', '').replace('")', '');
-      imgs.push(path);
-      console.log(path);
-    }
-
-    if (img) {
-      var path = $this.attr('src');
-        console.log(path);
-      if (path) {
-        imgs.push(path);
-      }
-    }
-  });
-
-  var percentsTotal = 1;
-
-  for (var i = 0; i < imgs.length; i++) {
-    var image = $('<img>', {
-      attr: {
-        src: imgs[i]
-      }
-    });
-
-    image.on({
-      load : function () {
-        setPercents(imgs.length, percentsTotal);
-        percentsTotal++;
-      },
-
-      error : function () {
-        percentsTotal++;
-      }
-    });
-  }
-
-  function setPercents(total, current) {
-    var percent = Math.ceil(current / total * 100);
-
-    if (percent >= 100) {
-      $('.preloader').fadeOut();
-    }
-
-    $('.preloader__percents').text(percent + '%');
-  }
-});
-
 //
-// (function( $ ){
-//   $.fn.myPluginName = function() {
-//     var imgs = [];
 //
-//     $.each($('*'), function () {
-//       var
-//           $this = $(this),
-//           background = $this.css('background-image'),
-//           img = $this.is('img');
+// var preloader = (function () {
+//   var
+//       imgs = [],
+//       percentsTotal = 1,
+//       _findUrlImg = function (){
+//         $.each($('*'), function () {
+//           var
+//               $this = $(this),
+//               background = $this.css('background-image'),
+//               img = $this.is('img');
 //
-//       if (background != 'none') {
-//         var path = background.replace('url("', '').replace('")', '');
-//         imgs.push(path);
-//         console.log(path);
-//       }
+//           if (background != 'none') {
+//             var path = background.replace('url("', '').replace('")', '');
+//             imgs.push(path);
+//           }
 //
-//       if (img) {
-//         var path = $this.attr('src');
-//         console.log(path);
-//         if (path) {
-//           imgs.push(path);
+//           if (img) {
+//             var path = $this.attr('src');
+//             if (path) {
+//               imgs.push(path);
+//             }
+//           }
+//         });
+//         imgTotal = imgs.length;
+//         return imgs
+//       },
+//       _PerCents = function() {
+//         for (var i = 0; i < imgs.length; i++) {
+//           var image = $('<img>', {
+//             attr: {
+//               src: imgs[i]
+//             }
+//           });
+//
+//           image.on({
+//             load : function () {
+//               setPercents(imgs.length, percentsTotal);
+//               percentsTotal++;
+//             },
+//
+//             error : function () {
+//               percentsTotal++;
+//             }
+//           });
 //         }
-//       }
-//     });
-//   return imgs
-//   };
-// })( jQuery );
+//       },
+//       setPercents = function () {
+//         var percent = Math.ceil(percentsTotal * 100 / imgTotal );
+//         // console.log(percentsTotal);
+//         if (percent >= 100) {
+//           $('.preloader').fadeOut();
+//         }
 //
-
-
-// var myPluginName = function () {
+//         $('.preloader__percents').text(percent + '%');
+//       },
+//       init = function () {
+//         _findUrlImg();
+//         _PerCents();
 //
-//   var imgs = [];
+//       };
+//   init();
 //
-//   $.each($('*'), function () {
-//     var
-//         $this = $(this),
-//         background = $this.css('background-image'),
-//         img = $this.is('img');
-//
-//     if (background != 'none') {
-//       var path = background.replace('url("', '').replace('")', '');
-//       imgs.push(path);
-//       console.log(path);
-//     }
-//
-//     if (img) {
-//       var path = $this.attr('src');
-//       if (path) {
-//         imgs.push(path);
-//       }
-//     }
-//   });
-//   return imgs;
-// }();
-//
-// // var qq = myPluginName();
-//
-// console.log(imgs);
+// }());
 
 /*form*/
 
@@ -169,7 +116,7 @@ var validation = (function () {
       _addError = function (element) {
         element.addClass('has-error');
         _createQtip(element, element.data('position'));
-        console.log('Error');
+        // console.log('Error');
       },
       _clearForm = function (e) { // Очищает форму
         var $form = $(this);
@@ -244,7 +191,7 @@ validation.init();
     var $form = $(this),
         url = '',
         defObject = ajaxForm($form, url);
-    console.log('submit');
+    // console.log('submit');
 
   }
 
@@ -263,7 +210,7 @@ validation.init();
   function publicInteface() {
     my = {
       publicMethod: function () {
-        console.log('Публичный метод');
+        // console.log('Публичный метод');
       }
     }
   }
@@ -271,3 +218,65 @@ validation.init();
   w.formQtp = my;
 
 })(jQuery, window);
+
+
+/*slider*/
+
+$(document).ready(function(e){
+  var sliderObject ={
+    name1: 'САЙТ ШКОЛЫ ОНЛАЙН ОБРАЗОВАНИЯ',
+    name2: 'САЙТ ШКОЛЫ ОНЛАЙН ОБРАЗОВАНИЯ',
+    name3: 'САЙТ ШКОЛЫ ОНЛАЙН ОБРАЗОВАНИЯ',
+    name4: 'САЙТ ШКОЛЫ ОНЛАЙН ОБРАЗОВАНИЯ',
+    image1: './assets/img/pic/work-1.png',
+    image2: './assets/img/pic/work-2.png',
+    image3: './assets/img/pic/work-3.png',
+    image4: './assets/img/pic/work-4.png',
+    technologies1: 'HTML, CSS, JAVASCRIPT',
+    technologies2: 'HTML, CSS, JAVASCRIPT',
+    technologies3: 'HTML, CSS, JAVASCRIPT',
+    technologies4: 'HTML, CSS, JAVASCRIPT'
+  };
+  var item = ['name1','name2','name3','name4'];
+  var itemImg = ['image1','image2','image3','image4'];
+  var itemTechnologies = ['technologies1','technologies2','technologies3','technologies4'];
+  var sliderTitle = $('.work__title');
+  var sliderImg = $('.slider__img');
+  var sliderTechnologies = $('.work__technology');
+  var i = 0;
+
+
+  $('.slider__button-right').on('click', function(){
+    setTimeout(function(){
+      sliderTitle.removeClass('bounceInDown');
+      sliderImg.removeClass('bounceInUp');
+    }, 1000);
+    i++;
+    if(i > item.length-1){
+      i = 0;
+    }
+
+    sliderImg.attr('src', sliderObject[itemImg[i]]);
+    sliderTitle.text(sliderObject[item[i]]);
+    sliderTechnologies.text(sliderObject[itemTechnologies[i]]);
+    sliderTitle.addClass('bounceInDown');
+    sliderImg.addClass('bounceInUp');
+  });
+
+  $('.slider__button-left').on('click', function(){
+    i--;
+    if(i < 0){
+      i = item.length-1;
+    }
+    setTimeout(function(){
+      sliderTitle.removeClass('bounceInDown');
+      sliderImg.removeClass('bounceInUp');
+    }, 1000);
+    sliderImg.attr('src', sliderObject[itemImg[i]]);
+    sliderTitle.text(sliderObject[item[i]]);
+    sliderTechnologies.text(sliderObject[itemTechnologies[i]]);
+    sliderTitle.addClass('bounceInDown');
+    sliderImg.addClass('bounceInUp');
+  });
+
+});
